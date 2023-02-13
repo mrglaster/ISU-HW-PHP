1294ec05934d4bf14419f15ea0b95fca94480030
Egor Pristavka <pristavkaegor03@gmail.com>
2023-02-13 19:06:59 +0800

    Delete Calls-Journal-Generation directory

--- Calls-Journal-Generation/test_data_adding.php
+++
@@ -1,6 +0,0 @@
-<?php
-$db = new PDO("mysql:dbname=calls_table;host=localhost", "testadmin", "testadmin");
-$db->query("use calls_table");
-$sql = "insert into  calls (id, phone_init, phone_receiver, duration, date) values  (4,".'"'."88003412345".'"'.','.'"'."89874617264".'"'.','.'"'."2.11".'"'.','.'"'."01-09-2003".'"'.");";
-$db->prepare($sql)->execute();
-echo "Test line was added!\n";
