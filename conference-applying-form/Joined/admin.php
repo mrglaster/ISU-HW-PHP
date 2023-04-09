c67f5048a197a9ff13154017f2839ea216057199
Egor Pristavka <pristavkaegor03@gmail.com>
2023-04-09 20:20:24 +0800

    Update admin.php

--- conference-applying-form/Joined/admin.php
+++ conference-applying-form/Joined/admin.php
@@ -20,8 +20,8 @@ $dir = 'applications/'; // Папка с файлами заявок
 $files = array_diff(scandir($dir), array('..', '.')); // Получение списка файлов
 
 if (count($files) > 0) { // Если есть файлы заявок
-    echo '<table>'; // Начало таблицы
-    echo '<tr><th>Имя</th><th>Фамилия</th><th>Email</th><th>Телефон</th><th>Тема</th><th>Метод оплаты</th><th>Отправлять информацию</th><th></th></tr>'; // Заголовок таблицы
+    echo '<form method="post" action="delete.php"><table>'; // Начало формы и таблицы
+    echo '<tr><th>Имя</th><th>Фамилия</th><th>Email</th><th>Телефон</th><th>Тема</th><th>Метод оплаты</th><th>Отправлять информацию</th><th><input type="checkbox" id="select-all"></th></tr>'; // Заголовок таблицы и колонка для чекбоксов
     foreach ($files as $file) { // Перебор файлов
         $data = file($dir . $file, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES); // Чтение данных из файла
         echo '<tr>'; // Начало строки таблицы
@@ -29,12 +29,21 @@ if (count($files) > 0) { // Если есть файлы заявок
             $value = explode(': ', $line); // Разбиваем строку на имя и значение
             echo '<td>' . $value[1] . '</td>'; // Отображаем значение
         }
-        echo '<td><form method="post" action="delete.php"><input type="hidden" name="file" value="' . $file . '"><input type="submit" value="Удалить заявку"></form></td>'; // Добавляем кнопку удаления
+        echo '<td><input type="checkbox" name="delete[]" value="' . $file . '"></td>'; // Добавляем чекбокс для удаления
         echo '</tr>'; // Конец строки таблицы
     }
-    echo '</table>'; // Конец таблицы
+    echo '<tr><td colspan="8"><input type="submit" value="Удалить выбранные заявки"></td></tr></table></form>'; // Добавляем кнопку удаления и закрываем форму и таблицу
 } else {
     echo 'Нет заявок'; // Если нет файлов заявок
 }
-
 ?>
+
+<script>
+    // Функция для выделения всех чекбоксов при нажатии на главный чекбокс
+    document.getElementById("select-all").addEventListener("click", function(){
+        var checkboxes = document.getElementsByName("delete[]");
+        for(var i = 0; i < checkboxes.length; i++) {
+            checkboxes[i].checked = this.checked;
+        }
+    });
+</script>
