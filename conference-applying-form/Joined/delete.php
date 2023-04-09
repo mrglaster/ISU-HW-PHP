1771247a4bf5cff31ce78d47c5a922d4999b2019
Egor Pristavka <pristavkaegor03@gmail.com>
2023-04-09 20:20:42 +0800

    Update delete.php

--- conference-applying-form/Joined/delete.php
+++ conference-applying-form/Joined/delete.php
@@ -1,15 +1,12 @@
 <?php
-
-if ($_SERVER['REQUEST_METHOD'] == 'POST') { // Если запрос был отправлен методом POST
-    $dir = 'applications/'; // Папка с файлами заявок
-    $file = $_POST['file']; // Получаем имя файла заявки
-    if (file_exists($dir . $file)) { // Если файл существует
-        unlink($dir . $file); // Удаляем файл
-        header('Location: admin.php'); // Перенаправляем обратно на страницу администратора
-        exit();
-    } else {
-        echo 'Файл не найден'; // Если файл не найден
+if (isset($_POST['delete'])) {
+    $files = $_POST['delete'];
+    foreach ($files as $file) {
+        if (file_exists('applications/' . $file)) {
+            unlink('applications/' . $file);
+        }
     }
 }
-
+header('Location: index.php');
+exit;
 ?>
